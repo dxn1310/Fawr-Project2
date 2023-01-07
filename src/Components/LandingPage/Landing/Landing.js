bd224241c3dba8e5eca58b4190c51afe448a28f6
Dhanish <siddharthbandaru20bcs75@iiitkottayam.ac.in>
2023-01-07 19:46:52 +0530

    half of media query completed1

--- src/Components/LandingPage/Landing/Landing.js
+++ src/Components/LandingPage/Landing/Landing.js
@@ -48,7 +48,7 @@ export default function Landing() {
             <div className='landing-inner'>
                 <Trust />
             </div>
-            <Reach />
+            {/* <Reach /> */}
         </div>
     )
 }
